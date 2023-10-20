6a4a61b26ba1077ea87c4ca05d64b9c845f60cac
Alex Godbehere <alex.godbehere@gmail.com>
2023-10-20 11:36:59 +0100

    Refine application configuration and setup instructions
    This commit modifies the '.env.example', 'README.md', 'docker-compose.yml', and 'get-keytab.sh' files for improved clarity and correctness. The changes include revising environment parameters for accuracy, amending the '.env.example' file for better configuration settings, updating the 'README.md' for precise setup instructions, refining the 'docker-compose.yml' to correctly reference keytabs, and revamping the 'get-keytab.sh' script for more reliable keytab fetching. These changes enhance code readability and maintainability while providing a more clear setup process for new users.

--- get-keytab.sh
+++ get-keytab.sh
@@ -5,4 +5,4 @@
 
 # bin/bash
 tmpfile=$(mktemp)
-export KEYTAB_PATH="$(kubectl --kubeconfig ~/.kube/k3s.yaml get -n factory-plus secret manager-keytab -o jsonpath="{.data.client-keytab}" | base64 -d >"$tmpfile" && echo "$tmpfile")"
+echo kubectl --kubeconfig ~/.kube/k3s.yaml get -n factory-plus secret manager-keytab -o jsonpath="{.data.client-keytab}" | base64 -d >"$tmpfile" && echo "$tmpfile"
